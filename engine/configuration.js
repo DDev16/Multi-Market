8efa4acf8735fb21f51d9522c04ed6111c50b62f
DDev16 <dillondevera12@gmail.com>
2023-01-05 22:15:25 -0700

    first commit

---
+++ engine/configuration.js
@@ -0,0 +1,80 @@
+import { NFTStorage } from "nft.storage";
+
+export const client = new NFTStorage({
+  token: process.env.NEXT_PUBLIC_NFTSTORAGE_API_KEY,
+});
+
+/*
+Songbird Testnet ---updated addresses 
+*/
+export var hhresell = "0x95bE842Cd440e8aA9d5bD93Ec12330c436e5D273";
+export var hhnftcol = "0xEbeB6f34ABFe1BB09F22446a34A489ff97c70411";
+export var hhnft = "0xB72381461f81E4DbF27eAa883E805a0757576eF7";
+export var hhmarket = "0x2Cc9701f59fF7CAe2bad43C8c933A725816157b6";
+export var hhrpc = "https://songbird.towolabs.com/rpc";
+
+/*
+Flare Mainnet
+*/ 
+export var flrresell = "0x02d6480848a386384A00FDE469D5C10926E51C67";
+export var flrnftcol = "0x7B6fF7a51a62f6F9dAc586aF1011A973288dEF61";
+export var flrnft = "0xD5d496aa89f94D06c8605aDCd38eD3CA896Df6Fa";
+export var flrmarket = "0xa6BFbefB1FC536b0825eD11996CF148C24054582";
+export var flrrpc = "https://songbird.towolabs.com/rpc";
+
+
+/*
+Eth Mainnet
+*/
+export var ethresell = "0xD6bEa3f2f34a44DA703f3514b3Ce7aA7EdD19fa0";
+export var ethnftcol = "0xC6e16c3ce24120B70f3967D1b8ad364577F70AAf";
+export var ethnft = "0x6FfF137677FEBee150628E767E926444218aBF9a";
+export var ethmarket = "0x56A7BE395206d86520a326E761DE551A827013DA";
+export var ethrpc = "https://rpc.ankr.com/eth_goerli";
+
+/*
+Binance Mainnet -- updated addresses 
+*/
+export var bnbresell = "0xc7A30d15Fe24032b8b700c0dCf71400Df3b92970";
+export var bnbnftcol = "0x382d4b1e396c6666640cc628b89ee0f5aec14994";
+export var bnbnft = "0x51A1c8956c038699423ab7A0726d7311c81f6F8f";
+export var bnbmarket = "0x33B3E5cF732CeE31a5f59243a54C490b90e9997F";
+export var bnbrpc = "https://bsc-dataseed2.defibit.io";
+
+
+/*
+Polygon Mainnet
+*/
+export var polyresell = "0xc7A30d15Fe24032b8b700c0dCf71400Df3b92970";
+export var polynftcol = "0x382D4B1e396c6666640Cc628B89Ee0f5Aec14994";
+export var polynft = "0x51A1c8956c038699423ab7A0726d7311c81f6F8f";
+export var polymarket = "0x33B3E5cF732CeE31a5f59243a54C490b90e9997F";
+export var polyrpc = "https://polygon.llamarpc.com";
+
+
+/*
+Goerli Testnet
+*/
+export var goeresell = "0xD6bEa3f2f34a44DA703f3514b3Ce7aA7EdD19fa0";
+export var goenftcol = "0xC6e16c3ce24120B70f3967D1b8ad364577F70AAf";
+export var goenft = "0x6FfF137677FEBee150628E767E926444218aBF9a";
+export var goemarket = "0x56A7BE395206d86520a326E761DE551A827013DA";
+export var goerpc = "https://rpc.ankr.com/eth_goerli";
+
+/*
+BSC Testnet
+*/
+export var bsctresell = "0x7aBFb92BBF795B22796B2C86709b4b40E9E90e95";
+export var bsctnftcol = "0x7634E8176bd3D6156dB154bf4df237ca184b6C9f";
+export var bsctnft = "0x60e6959a8652996D835055813A8b8c02FCa3E2E0";
+export var bsctmarket = "0xfC5583Bb036698cCda08199a4c8D291d76fAbffB";
+export var bsctrpc = "https://data-seed-prebsc-1-s3.binance.org:8545/";
+
+/*
+Mumbai Testnet
+*/
+export var mmresell = "0x935feEaA1e4959C7DeB9C3AF0B3B75C2b7e09d70";
+export var mmnftcol = "0x65f4643394437E0D27a151AB6e0ebbd72019089F";
+export var mmnft = "0x3dea30E978E8D042FdA3FF468665b0fd36720fBb";
+export var mmmarket = "0x2C35b5366011fa9B3531fef52d8D21c575153c0A";
+export var mmrpc = "https://matic-mumbai.chainstacklabs.com";
---
+++ pages/collection.js
@@ -0,0 +1,421 @@
+import { Card, Row, Button, Text, Container, Grid, Input, } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { ethers } from "ethers";
+import { goerpc, goeresell, goenftcol, bsctrpc, bsctresell, bsctnftcol, mmrpc, mmresell, mmnftcol, hhnftcol, hhrpc, hhresell,} from "../engine/configuration";
+import { bnbrpc, bnbresell, bnbnftcol } from "../engine/configuration";
+import { polyrpc, polyresell, polynftcol } from "../engine/configuration";
+import { simpleCrypto } from "../engine/configuration";
+import NFTCollection from "../engine/NFTCollection.json";
+import Resell from "../engine/Resell.json";
+import axios from "axios";
+import LoadingPopup from "../components/LoadingPopup";
+import Web3Modal from "web3modal";
+import { useRouter } from "next/router";
+
+const CustomCard = ({ nft, visible, setVisible, key, contractConfig }) => {
+  // var owner = user;
+  const { nftRpc, nftCol, nftResell } = contractConfig;
+  console.log("nft", nft);
+  const [resalePrice, updateresalePrice] = useState({ price: "" });
+
+  const router = useRouter();
+
+  async function executeRelist() {
+    const { price } = resalePrice;
+    if (!price) return;
+    try {
+      relistNFT();
+    } catch (error) {
+      console.log("Transaction Failed", error);
+    }
+  }
+  async function relistNFT() {
+    setVisible(true);
+    // var resell = nftresell;
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const price = ethers.utils.parseUnits(resalePrice.price, "ether");
+    const contractnft = new ethers.Contract(nftCol, NFTCollection, signer);
+    await contractnft.setApprovalForAll(nftResell, true).catch(() => {
+      setVisible(false);
+    });
+    let contract = new ethers.Contract(nftResell, Resell, signer);
+    let listingFee = await contract.getListingFee();
+    listingFee = listingFee.toString();
+    let transaction = await contract
+      .listSale(nft.tokenId, price, {
+        value: listingFee,
+      })
+      .catch(() => {
+        setVisible(false);
+      });
+    if (!transaction) {
+      return;
+    }
+    await transaction.wait();
+    router.push("/");
+  }
+
+  async function buylistNft() {
+    setVisible(true);
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftResell, Resell, signer);
+    const transaction = await contract
+      .buyNft(nft.tokenId, {
+        value: nft.cost,
+      })
+      .catch(() => {
+        setVisible(false);
+      });
+    if (!transaction) {
+      return;
+    }
+    await transaction.wait();
+    router.push("/portal");
+  }
+
+  return (
+    <Card
+      css={{
+        marginRight: "15px",
+        boxShadow: "1px 1px 10px #ffffff",
+        marginBottom: "15px",
+        height: "100%",
+      }}
+      variant="bordered"
+    >
+      {/* <Text
+        style={{
+          color: "white",
+          fontWeight: "bold",
+          fontFamily: "SF Pro Display",
+          fontWeight: "200",
+          fontSize: "20px",
+          marginLeft: "3px",
+        }}
+      >
+        {nft.name} Token-{nft.tokenId}
+      </Text> */}
+      <Card.Body css={{ p: 0 }}>
+        <Card.Image
+          css={{
+            maxWidth: "100%",
+            // maxHeight: "150px",
+            borderRadius: "6%",
+          }}
+          src={nft.img}
+        />
+      </Card.Body>
+      <Card.Footer css={{ justifyItems: "flex-start" }}>
+        <Row
+          key={key}
+          css={{
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            // "@media screen and (min-width:1000px)": {
+
+            // },
+          }}
+          wrap="wrap"
+          // justify="space-between"
+          // align="center"
+        >
+          <Text
+            css={{
+              color: "#fff",
+              fontSize: "18px",
+              textTransform: "capitalize",
+              mb: "0",
+            }}
+            h4
+          >
+            {nft.name} Token-{nft.tokenId}
+          </Text>
+          <Text
+            css={{
+              fontSize: "16px",
+              textTransform: "capitalize",
+              color: "#cecece",
+              fontWeight: "100",
+              letterSpacing: "0px",
+              whiteSpace: "nowrap",
+              width: "100%",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+            p
+          >
+            {nft.desc}
+          </Text>
+          {nft.cost == 0 ? (
+            <>
+              <Input
+                size="sm"
+                type="number"
+                css={{
+                  marginTop: "$2",
+                  maxWidth: "120px",
+                  marginBottom: "$2",
+                  border: "$blue500",
+                }}
+                style={{
+                  color: "white",
+                  fontFamily: "SF Pro Display",
+                  fontWeight: "bolder",
+                  fontSize: "15px",
+                }}
+                placeholder="Set your price"
+                onChange={(e) =>
+                  updateresalePrice({
+                    ...resalePrice,
+                    price: e.target.value,
+                  })
+                }
+                label="set price"
+              />
+
+              <Button
+                size="sm"
+                color="gradient"
+                onPress={executeRelist}
+                css={{ fontSize: "16px", minWidth: "100%" }}
+                disabled={
+                  resalePrice.price.length && resalePrice.price > 0
+                    ? false
+                    : true
+                }
+              >
+                Relist for Sale
+              </Button>
+            </>
+          ) : (
+            <>
+              <Text
+                css={{
+                  fontSize: "20px",
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  justifyContent: "space-between",
+                  mb: "10px",
+                }}
+              >
+                {nft.val}{" "}
+                <img
+                  src="n2dr-logo.png"
+                  style={{
+                    width: "60px",
+                    height: "25px",
+                    marginTop: "4px",
+                  }}
+                />
+              </Text>
+              <Button
+                color="gradient"
+                css={{ fontSize: "16px", minWidth: "100%" }}
+                onPress={buylistNft}
+              >
+                Buy
+              </Button>
+            </>
+          )}
+        </Row>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+const Collection = () => {
+  const [activeChain, setActiveChain] = useState(null);
+  const [nftArray, setNftArray] = useState();
+  const [contractConfig, setContractConfig] = useState({
+    nftRpc: null,
+    nftCol: null,
+    nftResell: null,
+  });
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    window.ethereum.on("chainChanged", (chainId) => {
+      console.log("chainChanged", chainId);
+      setActiveChain(chainId);
+    });
+  }, []);
+
+  const detectChain = async () => {
+    const provider = await detectEthereumProvider();
+
+    const chainId = await provider.request({ method: "eth_chainId" });
+
+    setActiveChain(chainId);
+  };
+
+  const setContract = () => {
+    var goe = "0xE"; // Flare conversion 
+    var mm = "0x13881";
+    var bsct = "0x61"; 
+    var hh = "0x13"; //songbird conversion
+    var bnb = "0x38"; 
+    var poly = "0x89";
+    if (activeChain == goe) {
+      var nftcol = goenftcol;
+      var nftrpc = goerpc;
+      var nftresell = goeresell;
+    } else if (activeChain == mm) {
+      var nftcol = mmnftcol;
+      var nftrpc = mmrpc;
+      var nftresell = mmresell;
+    } else if (activeChain == bsct) {
+      var nftcol = bsctnftcol;
+      var nftrpc = bsctrpc;
+      var nftresell = bsctresell;
+    } else if (activeChain == hh) {
+      var nftcol = hhnftcol;
+      var nftrpc = hhrpc;
+      var nftresell = hhresell;
+    } else if (activeChain == bnb) {
+      var nftcol = bnbnftcol;
+      var nftrpc = bnbrpc;
+      var nftresell = bnbresell;
+    } else if (activeChain == poly) {
+      var nftcol = polynftcol;
+      var nftrpc = polyrpc;
+      var nftresell = polyresell;
+    }
+    setContractConfig({
+      nftCol: nftcol,
+      nftRpc: nftrpc,
+      nftResell: nftresell,
+    });
+  };
+
+  async function loadNftResell() {
+    const provider = new ethers.providers.JsonRpcProvider(
+      contractConfig.nftRpc
+    );
+    const contract = new ethers.Contract(
+      contractConfig.nftCol,
+      NFTCollection,
+      provider
+    );
+    const market = new ethers.Contract(
+      contractConfig.nftResell,
+      Resell,
+      provider
+    );
+    const itemArray = [];
+    await contract.totalSupply().then((result) => {
+      // console.log("result", result);
+      for (let i = 0; i < result; i++) {
+        var token = i + 1;
+        const owner = contract.ownerOf(token).catch(function (error) {
+          console.log("tokens filtered");
+        });
+        const rawUri = contract.tokenURI(token).catch(function (error) {
+          console.log("tokens filtered");
+        });
+        const Uri = Promise.resolve(rawUri);
+        const getUri = Uri.then((value) => {
+          if (value) {
+            // console.log("valueUri", value);
+            var cleanUri = value.replace(
+              "ipfs://",
+              "https://infura-ipfs.io/ipfs/"
+            );
+            // console.log("cleanUri1", cleanUri);
+            let metadata = axios.get(cleanUri).catch(function (error) {
+              console.log(error.toJSON());
+            });
+            return metadata;
+          }
+        });
+        getUri.then((value) => {
+          // console.log("abcd", value)
+          let rawImg = value.data.image;
+          var name = value.data.name;
+          var desc = value.data.description;
+          let image = rawImg.replace("ipfs://", "https://ipfs.io/ipfs/");
+          const price = market.getPrice(token);
+          Promise.resolve(price).then((_hex) => {
+            var salePrice = Number(_hex);
+            var txPrice = salePrice.toString();
+            Promise.resolve(owner).then((value) => {
+              // console.log("value123", value);
+              let ownerW = value;
+              let outPrice = ethers.utils.formatUnits(
+                salePrice.toString(),
+                "ether"
+              );
+              let meta = {
+                name: name,
+                img: image,
+                cost: txPrice,
+                val: outPrice,
+                tokenId: token,
+                wallet: ownerW,
+                desc,
+              };
+              // console.log(meta);
+              itemArray.push(meta);
+            });
+          });
+        });
+      }
+    });
+    await new Promise((r) => setTimeout(r, 3000));
+    setNftArray(itemArray);
+  }
+
+  useEffect(() => {
+    detectChain();
+    setContract();
+  }, [activeChain]);
+
+  useEffect(() => {
+    const { nftCol, nftRpc, nftResell } = contractConfig;
+    if (nftCol && nftRpc && nftResell) {
+      loadNftResell();
+    }
+  }, [contractConfig]);
+
+  useEffect(() => {
+    console.log("contractConfig", contractConfig);
+  }, [contractConfig]);
+
+  useEffect(() => {
+    console.log("nftArray", nftArray);
+  }, [nftArray]);
+
+  const nftList = nftArray?.map((nft, i) => {
+    return (
+      <Grid className="SSSS" xs={12} sm={4} md={3} key={i}>
+        <CustomCard
+          nft={nft}
+          visible={visible}
+          setVisible={setVisible}
+          contractConfig={contractConfig}
+        />
+      </Grid>
+    );
+  });
+
+  return (
+    <>
+      <Container sm css={{ mt: "50px", mb: "50px" }}>
+        <Grid.Container gap={1} justify="flex-start">
+          {nftList}
+        </Grid.Container>
+        <LoadingPopup visible={visible} setVisible={setVisible} />
+      </Container>{" "}
+    </>
+  );
+};
+
+export default Collection;
